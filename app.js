1f3b7eed1a711ebae2ec1e2f10eae97f719e5a1b
bofer311 <ferbogado59@gmail.com>
2023-04-07 18:57:41 -0300

    Nuevo repositorio

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+
+
+
+  $("document.getElementById").ready(function() {
+  $("document.getElementById").mouseenter(function() {
+    $("#formulario").hide();
+  });
+
+
+  });
+
+  $(function(){
+
+  $(".btn btn-primary").click(function(){
+
+    alert($("comentario").html());
+  });
+  });
+
+});
