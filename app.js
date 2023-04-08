935a3ac6061198dc355bf50703f08536d5b6ffce
bofer311 <ferbogado59@gmail.com>
2023-04-08 19:00:37 -0300

    Se agrega API tiempo

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
 
 
 
-  $("document.getElementById").ready(function() {
-  $("document.getElementById").mouseenter(function() {
-    $("#formulario").hide();
-  });
+//   $("document.getElementById").ready(function() {
+//   $("document.getElementById").mouseenter(function() {
+//     $("#formulario").hide();
+//   });
 
 
-  });
+//   });
 
-  $(function(){
+//   $(function(){
 
-  $(".btn btn-primary").click(function(){
+//   $(".btn btn-primary").click(function(){
 
-    alert($("comentario").html());
-  });
-  });
+//     alert($("comentario").html());
+//   });
+//   });
 
-});
+// });
+
+var formVisible = false;
+var button = document.querySelector('button');
+
+function showForm() {
+  var form = document.getElementById('Formulario');
+  form.style.display = 'block';
+  button.textContent = 'Doble click para ocultar el formulario';
+  button.removeEventListener('click', showForm);
+  button.addEventListener('dblclick', hideForm);
+  formVisible = true;
+}
+
+// function hideForm() {
+//   var form = document.getElementById('Formulario');
+//   form.style.display = 'none';
+//   button.textContent = 'Contactenos!';
+//   button.removeEventListener('dblclick', hideForm);
+//   button.addEventListener('click', showForm);
+//   formVisible = false;
+// }
